175fb9ac771beab8b5d938c2591f5ad1c50a0467
Gabriel Aedo <aedothegabriel@gmail.com>
2025-12-31 19:00:12 -0500

    Primer avance (header)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://GabrielAedoPozo.github.io',
-    base: 'bandido_jeans'
+    base: 'bandido_jeans',
+    
+    vite: {
+      plugins: [tailwindcss()],
+    },
+
+
 });
