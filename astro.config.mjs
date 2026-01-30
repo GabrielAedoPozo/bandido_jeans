2056443c2b1a08f632c2bb3d7bcfb5d338dad390
Gabriel Aedo <aedothegabriel@gmail.com>
2026-01-30 15:39:43 -0500

    fix: astro static build for cloudflare pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,10 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
-// https://astro.build/config
 export default defineConfig({
-    site: 'https://GabrielAedoPozo.github.io',
-    base: 'bandido_jeans',
-    
-    vite: {
-      plugins: [tailwindcss()],
-    },
+  output: 'static',
 
-
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
