8ac052b9b8015f9cd736f08419d44edd56632ea8
Gabriel Aedo <aedothegabriel@gmail.com>
2026-01-31 15:01:53 -0500

    seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,17 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  site: 'https://bandidojeans.com',
   output: 'static',
-
+  compressHTML: true,
+  build: {
+    inlineStylesheets: 'auto',
+  },
   vite: {
     plugins: [tailwindcss()],
+    build: {
+      cssMinify: true,
+      minify: 'terser',
+    },
   },
 });
