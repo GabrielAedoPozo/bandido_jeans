b43246aa38eb2834ab6d83a96b9e60f7c5b64090
Gabriel Aedo <aedothegabriel@gmail.com>
2026-02-07 12:37:49 -0500

    arreglo del sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  site: 'https://bandidojeans.com',
+  site: 'https://bandidosjeans.com',
   output: 'static',
   compressHTML: true,
   build: {
